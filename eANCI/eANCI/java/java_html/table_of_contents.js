7f57656f2435df3ce4a58849e9746085cec590cf
Evan Montgomery-Recht <montge@mianetworks.net>
2025-03-02 14:17:39 -0500

    Documentation Analysis and code for prepping eTOUR

---
+++ eANCI/eANCI/java/java_html/table_of_contents.js
@@ -0,0 +1,30 @@
+var ToC = `
+<div id="main-dropdown" class="dropdown" tabindex="0">
+<button id="dropdown-button" class="dropbtn"><span>Contents </span><img id="dropdown-icon" src="ToCdropdown.png" style="height:12px"></button>
+<div id="dropdown-buffer" class="dropdown-border" style="height:120px;width:300px;"></div><div id="nav-dropdown-content" class="dropdown-content"><a href="projectoverview.html">Overview</a>
+<a href="dictionary_A.html">Data Dictionary</a>
+<a href="contents_file_E.html">File Contents</a>
+<a href="progunit_xref_A.html">Program Unit Cross Reference</a>
+<a href="object_xref_A.html">Object Cross Reference</a>
+<a href="type_xref_A.html">Type Cross Reference</a>
+<a href="decltree_&.html">Declaration Tree</a>
+<a href="simpleinvtree_B.html">Simple Invocation Tree</a>
+<a href="import_E.html">Imports</a>
+<a href="progunitcomp_metrics_B.html">Program Unit Complexity</a>
+<a href="projmetrics.html">Project Metrics</a>
+<a href="progunit_metrics_B.html">Program Unit Metrics</a>
+<a href="file_metrics_E.html">File Metrics</a>
+<a href="fileam_E.html">File Average Metrics</a>
+<a href="classm_B.html">Class Metrics</a>
+<a href="classoom_B.html">Class OO Metrics</a>
+<a href="uninitializeditems_E.html">Uninitialized Items</a>
+<a href="unuseditemsall_E.html">Unused Objects and Functions</a>
+<a href="unusedobj_E.html">Unused Objects</a>
+<a href="unusedtype_E.html">Unused Types</a>
+<a href="unusedprogunit_E.html">Unused Program Units</a>
+<a href="entity_index.html">Index</a>
+<a href="entity_tree_index.html">Tree Index</a>
+
+</div>
+</div>
+`
